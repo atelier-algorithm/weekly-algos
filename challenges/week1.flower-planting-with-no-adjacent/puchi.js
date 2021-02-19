261327fd57125cca795d99404c1291d31e56bbc5
puchidev <puchimonoo@gmail.com>
2021-02-20 04:27:58 +0900

    prune meaningless code in flower planting prob

--- challenges/week1.flower-planting-with-no-adjacent/puchi.js
+++ challenges/week1.flower-planting-with-no-adjacent/puchi.js
@@ -1,6 +1,42 @@
+// v2
+// Runtime: 168 ms, faster than 70.51% of JavaScript online submissions for Flower Planting With No Adjacent.
+// Memory Usage: 58.7 MB, less than 23.08% of JavaScript online submissions for Flower Planting With No Adjacent.
+const gardenNoAdj = (n, paths) => {
+  // build graph
+  const graph = {};
+  // add vertices
+  for (let i = 0; i < n; i += 1) graph[i + 1] = [];
+  // add bidirectional edges
+  for (const [from, to] of paths) {
+    graph[from].push(to);
+    graph[to].push(from);
+  }
+
+  const answer = [];
+
+  // for each garden...
+  for (let garden = 1; garden <= n; garden += 1) {
+    // list flowers planted nearby
+    const nearby = {};
+    for (const neighbor of graph[garden]) {
+      const flower = answer[neighbor - 1];
+      if (flower) nearby[flower] = true;
+    }
+    // find unused flower and assign it to the answer
+    // 4 = maximum number of paths + 1
+    for (let flower = 1; flower <= 4; flower += 1)
+      if (!nearby[flower]) {
+        answer[garden - 1] = flower;
+        break;
+      }
+  }
+
+  return answer;
+};
+
+/* v1
 // Runtime: 196 ms, faster than 57.69% of JavaScript online submissions for Flower Planting With No Adjacent.
 // Memory Usage: 59 MB, less than 17.95% of JavaScript online submissions for Flower Planting With No Adjacent.
-
 const gardenNoAdj = (n, paths) => {
   // build graph
   const graph = {};
@@ -33,6 +69,7 @@ const gardenNoAdj = (n, paths) => {
 
   return answer;
 };
+*/
 
 describe('flower-planting-with-no-adjacent', () => {
   test.each`
@@ -43,6 +80,6 @@ describe('flower-planting-with-no-adjacent', () => {
     ${5} | ${[[4, 1], [4, 2], [4, 3], [2, 5], [1, 2], [1, 5]]} | ${[1, 2, 1, 3, 3]}
     ${5} | ${[[3, 4], [4, 5], [3, 2], [5, 1], [1, 3], [4, 2]]} | ${[1, 1, 2, 3, 2]}
   `('returns $output from $input', ({ n, paths, output }) => {
-    expect(longestSubstring(n, paths)).toEqual(output);
+    expect(gardenNoAdj(n, paths)).toEqual(output);
   });
 });
