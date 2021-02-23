8740db1d0d0e57f976c76006d0d720c124465eaf
ikasty <mail.ikasty@gmail.com>
2021-02-23 21:26:42 +0900

    Faster version of group anagrams

--- challenges/week2.group-anagrams/ikasty.py
+++ challenges/week2.group-anagrams/ikasty.py
@@ -1,19 +1,9 @@
-# Runtime: 120 ms, faster than 29.53% of Python3 online submissions
-# Memory Usage: 25 MB, less than 6.09% of Python3 online submissions
+# Runtime: 88 ms, faster than 95.79% of Python3 online submissions
+# Memory Usage: 17.9 MB, less than 50.18% of Python3 online submissions
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        result_list = []
-        hashmap = {}
+        result = defaultdict(list)
         for item in strs:
-            target = hashmap
-            for c in sorted(item):
-                if c not in target:
-                    target[c] = {}
-                target = target[c]
+            result[''.join(sorted(item))].append(item)
 
-            if 0 not in target:
-                target[0] = []
-                result_list.append(target[0])
-            target[0].append(item)
-
-        return result_list
+        return result.values()
