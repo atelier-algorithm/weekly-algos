c7d2529e1dd45fa8d1352dc92001387c270b43b1
ikasty <mail.ikasty@gmail.com>
2021-02-27 00:23:49 +0900

    Apply reviews
    https://github.com/atelier-algorithm/weekly-algos/pull/4#pullrequestreview-598826103

--- challenges/week2.01-matrix/ikasty.scala
+++ challenges/week2.01-matrix/ikasty.scala
@@ -1,5 +1,5 @@
-// Runtime: 7084 ms, faster than 8.33% of Scala online submissions
-// Memory Usage: 64.1 MB, less than 25.00% of Scala online submissions
+// Runtime: 7052 ms, faster than 8.33% of Scala online submissions
+// Memory Usage: 62.3 MB, less than 50.00% of Scala online submissions
 import scala.collection.mutable.Queue
 
 object Solution {
@@ -8,31 +8,30 @@ object Solution {
         val queue = Queue[(Int, Int)]()
 
         // matrix에 0이 들어간 모든 좌표를 구한다
-        val init = (0 until matrix.size) flatMap {
-            i => (0 until matrix.head.size) map {
-                j => (i, j)
-            }
-        } filter { tuple => matrix(tuple._1)(tuple._2) == 0 }
+        val init =
+            for (i <- (0 until matrix.size); j <- (0 until matrix.head.size) if matrix(i)(j) == 0)
+                yield (i, j)
 
         // 해당하는 좌표의 result에 전부 0을 넣고, queue에도 넣는다
-        init.map( item => result(item._1)(item._2) = 0 )
+        init.foreach{ case (i, j) => result(i)(j) = 0 }
         queue ++= init
 
         while (!queue.isEmpty) {
-            val tuple = queue.dequeue
+            val (x, y) = queue.dequeue
             // 후보 좌표 중에서...
-            val candidate = List((tuple._1 - 1, tuple._2), (tuple._1 + 1, tuple._2),
-                                 (tuple._1, tuple._2 - 1), (tuple._1, tuple._2 + 1))
+            val candidate = List((x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1))
                 // matrix 범위 내에 있으며
-               .filter(item => item._1 >= 0 && item._2 >= 0)
-               .filter(item => item._1 < matrix.size && item._2 < matrix.head.size)
+               .filter(_._1 >= 0)
+               .filter(_._2 >= 0)
+               .filter(_._1 < matrix.size)
+               .filter(_._2 < matrix.head.size)
                 // 방문하지 않은 노드이며
-               .filter(item => result(item._1)(item._2) < 0)
+               .filter{ case (i, j) => result(i)(j) < 0 }
                 // 큐에 넣지 않은 노드라면
-               .filter(item => !queue.contains(item))
+               .filter(!queue.contains(_))
 
             // 현재 result에서 1 증가시키고 큐에 넣기
-            candidate map { item => result(item._1)(item._2) = result(tuple._1)(tuple._2) + 1 }
+            candidate.foreach{ case (i, j) => result(i)(j) = result(x)(y) + 1 }
             queue ++= candidate
         }
 
