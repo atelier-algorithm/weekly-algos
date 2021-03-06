e63d65187a5381a78f8f370fb73bff9cd5b41d0b
puchidev <puchimonoo@gmail.com>
2021-03-06 16:52:39 +0900

    avoid underscore dangling

--- challenges/week3.bag-of-tokens/puchi.js
+++ challenges/week3.bag-of-tokens/puchi.js
@@ -3,21 +3,21 @@
 
 const bagOfTokensScore = (tokens, P) => {
   // 토큰을 오름차순으로 정렬 (TypedArray를 써서 숫자 정렬을 빠르게 함)
-  const _tokens = new Uint16Array(tokens).sort();
+  const $tokens = new Uint16Array(tokens).sort();
   let left = 0;
-  let right = _tokens.length - 1;
+  let right = $tokens.length - 1;
   let score = 0;
 
   while (left <= right) {
     // 가장 작은 토큰들을 팔 수 있는 데까지 판매
-    while (P >= _tokens[left]) {
-      P -= _tokens[left++];
+    while (P >= $tokens[left]) {
+      P -= $tokens[left++];
       score += 1;
     }
     // 점수가 있다면 사용해서 가장 비싼 토큰을 구매
     // 마지막 토큰일 경우 구매하지 않고 절약
-    if (score && left < right && _tokens[left] <= P + _tokens[right]) {
-      P += _tokens[right--];
+    if (score && left < right && $tokens[left] <= P + $tokens[right]) {
+      P += $tokens[right--];
       score -= 1;
     } else break;
   }
