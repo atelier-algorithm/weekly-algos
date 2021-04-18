17032997149705eaf888c94dd9c3463408ca8135
puchidev <puchimonoo@gmail.com>
2021-04-18 18:05:36 +0900

    solve swim in rising water (DFS+BS)

--- challenges/week6.swim-in-rising-water/puchi.js
+++ challenges/week6.swim-in-rising-water/puchi.js
@@ -1,5 +1,5 @@
-// Runtime: 488 ms, faster than 29.69% of JavaScript online submissions for Swim in Rising Water.
-// Memory Usage: 45.8 MB, less than 28.13% of JavaScript online submissions for Swim in Rising Water.
+// Runtime: 100 ms, faster than 84.38% of JavaScript online submissions for Swim in Rising Water.
+// Memory Usage: 43.6 MB, less than 87.50% of JavaScript online submissions for Swim in Rising Water.
 
 const offsets = [
   [-1, 0],
@@ -9,13 +9,24 @@ const offsets = [
 ];
 
 function swimInWater(grid) {
-  let time = Math.max(2, grid[0][0]);
-  while (!canReach(grid, time)) ++time;
-  return time;
+  const N = grid.length;
+  let low = Math.max(2, grid[0][0]);
+  let high = N * N - 1;
+  let found;
+  while (low <= high) {
+    const mid = ((low + high) / 2) | 0;
+    if (canReachBottomRight(grid, mid)) {
+      found = mid;
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+  return found ?? Infinity;
 }
 
 // time만큼의 시간이 흐른 후 grid 우하단에 도착할 수 있는지 검사
-function canReach(grid, time, row = 0, col = 0, visited = {}) {
+function canReachBottomRight(grid, time, row = 0, col = 0, visited = {}) {
   const stop = grid.length - 1;
   // 그리드를 벗어남
   if (row < 0 || stop < row || col < 0 || stop < col) return false;
@@ -28,34 +39,37 @@ function canReach(grid, time, row = 0, col = 0, visited = {}) {
   visited[height] = true;
 
   for (const [drow, dcol] of offsets)
-    if (canReach(grid, time, row + drow, col + dcol, visited)) return true;
+    if (canReachBottomRight(grid, time, row + drow, col + dcol, visited)) return true;
 
   return false;
 }
 
 describe('swim-in-rising-water', () => {
-  it('should return minimum time required before to be able to swim from top left to bottom right', () => {
-    const grid1 = [
-      [0, 2],
-      [1, 3],
-    ];
-    expect(swimInWater(grid1)).toBe(3);
+  it.each(
+    'should return minimum time required before to be able to swim from top left to bottom right',
+    () => {
+      const grid1 = [
+        [0, 2],
+        [1, 3],
+      ];
+      expect(swimInWater(grid1)).toBe(3);
 
-    const grid2 = [
-      [0, 1, 2, 3, 4],
-      [24, 23, 22, 21, 5],
-      [12, 13, 14, 15, 16],
-      [11, 17, 18, 19, 20],
-      [10, 9, 8, 7, 6],
-    ];
-    expect(swimInWater(grid2)).toBe(16);
+      const grid2 = [
+        [0, 1, 2, 3, 4],
+        [24, 23, 22, 21, 5],
+        [12, 13, 14, 15, 16],
+        [11, 17, 18, 19, 20],
+        [10, 9, 8, 7, 6],
+      ];
+      expect(swimInWater(grid2)).toBe(16);
 
-    const grid3 = [
-      [10, 12, 4, 6],
-      [9, 11, 3, 5],
-      [1, 7, 13, 8],
-      [2, 0, 15, 14],
-    ];
-    expect(swimInWater(grid3)).toBe(14);
-  });
+      const grid3 = [
+        [10, 12, 4, 6],
+        [9, 11, 3, 5],
+        [1, 7, 13, 8],
+        [2, 0, 15, 14],
+      ];
+      expect(swimInWater(grid3)).toBe(14);
+    }
+  );
 });
